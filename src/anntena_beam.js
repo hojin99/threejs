617f0b479597a94e8729c636262e4bfce2809114
조호진 <hojin99@naver.com>
2022-05-02 16:49:40 +0900

    안테나 빔 패턴 도시

--- src/anntena_beam.js
+++ src/anntena_beam.js
@@ -20,7 +20,7 @@ class App {
 
         this._meshs = [];
         this._setupCamera();
-        this._setupLight();
+        // this._setupLight();
         this._setupModel();
         this._setupControls();   
         this._setupGrid();   
@@ -29,14 +29,16 @@ class App {
         window.onresize = this.resize.bind(this);
         this.resize();
 
+        // 브라우저에 애니메이션 프레임 요청 - render 호출됨
         requestAnimationFrame(this.render.bind(this));
     }
 
     _loadObj() {
-
+        // 외부 3차원 객체를 추가
         let object;
         let that = this;
 
+        // 진행 중 호출 함수
         function onProgress( xhr ) {
 
             if ( xhr.lengthComputable ) {
@@ -47,8 +49,10 @@ class App {
             }
         }
 
+        // 에러 시 호출 함수
         function onError() {}
 
+        // 최종 호출 함수
         function loadModel() {
             object.scale.set(0.0005,0.0005,0.0004)
             that._scene.add( object );
@@ -64,43 +68,49 @@ class App {
     }
 
     _setupGrid() {
-        const helper = new THREE.GridHelper( 100, 100 );
+        // 2차원 격자를 정의할 수 있도록 해줌
+        const helper = new THREE.GridHelper( 10, 100 ); // total size, divisions
         helper.material.opacity = 0.25;
         helper.material.transparent = true;
         this._scene.add( helper );
     }
 
     _setupControls() {
-        // 마우스 컨트롤을 가능하게 해줌
+        // 마우스 컨트롤에 따라서 Camera가 Target 주위를 회전할 수 있도록 해줌
         const control = new OrbitControls(this._camera, this._divContainer);
-        control.maxPolarAngle = Math.PI / 2;
+        control.maxPolarAngle = Math.PI / 2; // 수직으로 얼마나 각도를 움직일 수 있는 지 설정 0 ~ PI
     }
 
     _setupCamera() {
+        // 카메라 설정
         const width = this._divContainer.clientWidth;
         const height = this._divContainer.clientHeight;
         console.log(`[_setupCamera] width - ${width}`);
         console.log(`[_setupCamera] height - ${height}`);
+        // 원근감이 있는 카메라 (일반 적인 경우 이걸 사용하면 된다고 함)
         const camera = new THREE.PerspectiveCamera(
-            75,
-            width/height,
-            0.1,
-            100
+            75, // 시야각
+            width/height, // 종횡비
+            0.1, // near
+            100 // far
         );
-        camera.position.z = 2;
+        camera.position.z = 2; // 2정도 뒤로
+        camera.position.y = 1; // 1정도 위로
         this._camera = camera;
     }
 
     _setupLight() {
+        // 광원 설정
         const color = 0xffffff;
         const intensity = 1; // 광원세기
+        // 태양과 비슷한 성격의 광원, 그림자 사용 가능
         const light = new THREE.DirectionalLight(color, intensity);
-        light.position.set(-4, 4, 10);
+        light.position.set(-4, 4, 10); // 광원 위치
         this._scene.add(light);
     }
 
     _setupModel() {
-
+        // 모델 설정
         const vertices = [];
         const colors = [];
         const sizes = [];
@@ -141,14 +151,14 @@ class App {
 
 				} );
 
-                // const material = new THREE.PointsMaterial( { size: 0.03, vertexColors: true } );
+                //  const material = new THREE.PointsMaterial( { size: 0.03, vertexColors: true } );
         
                 const meshGeometry = new THREE.BufferGeometry();
                 meshGeometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
 				meshGeometry.setAttribute( 'customColor', new THREE.Float32BufferAttribute( colors, 3 ) );
                 // meshGeometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) );
 				meshGeometry.setAttribute( 'size', new THREE.Float32BufferAttribute( sizes, 1 ) );
-                // 초기 배치가 Z가 위가 아니라 앞 쪽이어서 위가 되도록 돌려줌
+                // 3D 모델의 초기 배치가 Z가 위가 아니라 앞 쪽이어서 위가 되도록 돌려줌
                 meshGeometry.rotateX( - Math.PI / 2 );
 
                 this._addPoint(meshGeometry, material, 0,0,0);
@@ -220,6 +230,7 @@ class App {
         this._camera.aspect = width/height;
         this._camera.updateProjectionMatrix(); 
 
+        // rnderer 사이즈를 맞춰줌
         this._renderer.setSize(width, height);
     }
 }
